836c564f2f645d627db02b201ea94de84992a891
TAKE72K <hyes941073@gmail.com>
2018-07-25 18:03:22 +0800

    加了一些小垃圾

--- MisakiAobaBot.py
+++ MisakiAobaBot.py
@@ -200,6 +200,41 @@ def which(bot, update, args):
             text="わたしは〜♬［$res］が良いと思うよ〜えへへ。".replace('$res',result)
             msg=bot.send_message(chat_id=update.message.chat_id, text=text)
 
+
+def dice(bot,update,args):
+    """Send a message when the command /dice is issued."""
+    dice=['⚀','⚁','⚂','⚃','⚄','⚅']
+    count=[0,0,0,0,0,0]
+    text=''
+    if update.message.date > init_time:
+        if not args:
+            #dice 1
+                msg=bot.send_message(chat_id=update.message.chat_id, text=dice[randrange(6)])
+        else:
+            dice_num=' '.join(args)
+            try:
+                num=int(dice_num)
+            except:
+                #value error
+                return
+            else:
+                if num>100:
+                    return
+                else:
+                    for i in range(0,num):
+                        j=randrange(6)
+                        text=text+dice[j]
+                        count[j]=count[j]+1
+                    msg=bot.send_message(chat_id=update.message.chat_id, text=text)
+                    text=''
+                    for i in range(0,6):
+                        text=text+dice[i]+str(count[i])+'個\n'
+                    if num>20:
+                        msg1=bot.send_message(chat_id=update.message.chat_id, text=text)
+                        time.sleep(5)
+                        bot.delete_message(chat_id=update.message.chat_id, message_id=msg.message_id)
+                        bot.delete_message(chat_id=update.message.chat_id, message_id=msg1.message_id)
+                    
 @run_async
 def tiger(bot, update):
     word_tiger_1="<pre>あー</pre>"
@@ -400,6 +435,7 @@ def main():
     dp.add_handler(CommandHandler("tiger", tiger))
     dp.add_handler(CommandHandler("notiger", notiger))
     dp.add_handler(CommandHandler("which", which, pass_args=True))
+    dp.add_handler(CommandHandler("dice", dice, pass_args=True))
     # dp.add_handler(CommandHandler("title", title, pass_args=True))
 
     # ---Message answer---
