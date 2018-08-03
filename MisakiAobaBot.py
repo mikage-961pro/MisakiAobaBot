8595bbf0b7fe8ba0a9bf61db4f150ec7b92d9500
TAKE72K <hyes941073@gmail.com>
2018-08-03 14:58:28 +0800

    ?:L

--- MisakiAobaBot.py
+++ MisakiAobaBot.py
@@ -163,7 +163,7 @@ def set_config(id,command):
             setting=setting+command
         worksheet.update_cell(cell.row,cell.col+1,setting)
 
-def get_config(id,setting):
+def get_config(id,setting='a'):
     scope = ['https://spreadsheets.google.com/feeds']
     creds = ServiceAccountCredentials.from_json_keyfile_name('auth.json', scope)
     client = gspread.authorize(creds)
@@ -555,7 +555,7 @@ def key_word_reaction(bot,update):
         find_word(words=['高木','社長','順二朗'], echo='あぁ！社長のことを知りたい！')
         find_word(words=['天海','春香'], echo='天海さんのクッキーはとっても美味しいですね〜')
         find_word(words=['閣下'], echo='え！？もしかして春香ちゃん！？',els='恐れ、平れ伏し、崇め奉りなさいのヮの！',prob=90)
-        find_word(words=['如月','千早'], echo='如月さんの歌は素晴らしい！')
+        find_word(words=['如月','千早'], echo='如月さんの歌は素晴らしい！',els='静かな光は蒼の波紋 VERTEX BLUE!!!!',prob=72)
         find_word(words=['72'],prob=10, echo='こんな言えば如月さんは怒ってしまうよ！')
         find_word(words=['星井','美希'], echo='あの...星井さんはどこかで知っていますか？')
         find_word(words=['高槻','やよい'], echo="ζ*'ヮ')ζ＜うっうー ")
@@ -563,9 +563,9 @@ def key_word_reaction(bot,update):
         find_word(words=['秋月','律子'], echo='律子さんは毎日仕事するで、大変ですよね〜')
         find_word(words=['三浦','あずさ'], echo='え？あずささんは今北海道に！？')
         find_word(words=['水瀬','伊織'], echo='このショコラは今朝水瀬さんからの、みな一緒に食べろう！')
-        find_word(words=['菊地','真'], echo='真さんは今、王子役の仕事をしていますよ。',allco=True)
-        find_word(words=['我那覇','響'], echo='ハム蔵はどこでしょうか？探していますね',allco=True)
-        find_word(words=['四条','貴音'], echo='昨日〜貴音さんがわたしに色々な美味しい麺屋を紹介しました！')
+        find_word(words=['菊地','真'], echo='真さんは今、王子役の仕事をしていますよ。',els='真さんは今、ヒーロー役の仕事をしていますよ～～激しい光は黒の衝撃 VERTEX BLACK!!!!',prob=70,allco=True)
+        find_word(words=['我那覇','響'], echo='ハム蔵はどこでしょうか？探していますね',els='弾ける光は浅葱の波濤 VERTEX LIGHTBLUE!!',prob=70,allco=True)
+        find_word(words=['四条','貴音'], echo='昨日〜貴音さんがわたしに色々な美味しい麺屋を紹介しました！',els='秘めたり光は臙脂の炎 VERTEX CARMINE〜〜',prob=70)
         find_word(words=['亜美'], echo='亜美？あそこよ')
         find_word(words=['真美'], echo='真美？いないよ')
         find_word(words=['双海'], echo='亜美真美？先に外へ行きました')
