d84b64bc458e5944fad80ac8d521e2571c879d4d
TAKE72K <hyes941073@gmail.com>
2018-08-15 14:00:53 +0800

    cursor

--- postgre.py
+++ postgre.py
@@ -5,6 +5,7 @@ from psycopg2 import sql
 DATABASE_URL = os.environ['DATABASE_URL']
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+curs=conn.cursor()
 def dbDump(table,data,col):
     q1 = sql.SQL("insert into {} ({}) values ({})").format(sql.Identifier(table),sql.SQL(', ').join(map(sql.Identifier, col)),sql.SQL(', ').join(sql.Placeholder() * len(col)))
 
