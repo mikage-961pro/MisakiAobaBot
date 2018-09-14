a5345d942a71dcc9957899e1d06afde7385e8821
TAKE72K <hyes941073@gmail.com>
2018-09-14 21:52:26 +0800

    mongo quote f

--- MisakiAobaBot.py
+++ MisakiAobaBot.py
@@ -181,12 +181,13 @@ def which(bot, update, args):
 def quote(bot,update):
     global config_buffer
     global config_buffer_Plock
+    xxx=False
     #daily quote
-    if get_config(update.message.from_user.id,'q',config_buffer,config_buffer_Plock)==True:
+    if xxx==True:
         del_cmd_func(bot,update)
         return
     else:
-        config_buffer=set_config(update.message.from_user.id,'q')
+        #config_buffer=set_config(update.message.from_user.id,'q')
         del_cmd_func(bot,update)
     quote=MisaMongo.randget()[0]
     text='<pre>'+quote[quote]+'</pre>\n'+'-----<b>'+quote[said]+'</b> より'
