34cc8c85215d698f577b6fa39ceb66835c7ab676
TAKE72K <hyes941073@gmail.com>
2018-09-15 10:39:04 +0800

    jj

--- MisakiAobaBot.py
+++ MisakiAobaBot.py
@@ -378,6 +378,9 @@ def key_word_reaction(bot,update):
     'https://i.imgur.com/b9s69iK.mp4',
     'https://img.gifmagazine.net/gifmagazine/images/1333179/original.mp4']
 
+    if update.message.reply_to_message.reply_markup is not None:
+        bot.send_message(chat_id=update.message.chat_id,text='true')
+    
     # word_echo
     if switch == True:
         find_word(words=['大老','dalao','ㄉㄚˋㄌㄠˇ','巨巨','Dalao','大 佬'],echo='你才大佬！你全家都大佬！', prob=200)
