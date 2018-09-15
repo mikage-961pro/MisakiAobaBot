148533a290b2c41798874960700747020d3edde6
TAKE72K <hyes941073@gmail.com>
2018-09-15 11:31:17 +0800

    jj

--- MisakiAobaBot.py
+++ MisakiAobaBot.py
@@ -21,7 +21,7 @@ from string import Template
 from functools import wraps
 
 # ---Telegram
-from telegram import Bot, Chat, Sticker, ReplyKeyboardMarkup
+from telegram import Bot, Chat, Sticker, ReplyKeyboardMarkup,MessageEntity
 from telegram import ReplyKeyboardRemove, ParseMode,ForceReply
 from telegram import InlineQueryResultArticle, InputTextMessageContent,InlineKeyboardMarkup,InlineKeyboardButton
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters,JobQueue,CallbackQueryHandler
