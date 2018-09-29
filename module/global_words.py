971bb50ab99b00d3bd66e1bac17b7e903b10a299
Dephilia <leedaniel682@gmail.com>
2018-09-29 21:19:49 +0800

    fix

--- module/global_words.py
+++ module/global_words.py
@@ -169,7 +169,7 @@ Database:MongoDB
     'nansu'
     ]
     daredemo=[
-    "DD"
+    'dd'
     ]
     idol_list=idol_list_pr+idol_list_fr+idol_list_ag+idol_list_al+zimuinn\
                 +idol_list_passion+try_sail+daredemo
